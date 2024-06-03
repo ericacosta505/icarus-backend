ae69f909b15dd08551c310c48a1f5873e22c784d
Eric Acosta <72636775+ericacosta505@users.noreply.github.com>
2024-06-02 19:25:27 -0700

    Update server.mjs

--- server/server.mjs
+++ server/server.mjs
@@ -10,12 +10,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 mongoose
