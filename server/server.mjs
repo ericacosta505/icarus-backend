16920644757f221379e96c5d483b28b0fdca8990
Eric Acosta <72636775+ericacosta505@users.noreply.github.com>
2024-06-02 19:11:00 -0700

    Update server.mjs

--- server/server.mjs
+++ server/server.mjs
@@ -25,7 +25,10 @@ mongoose
 
 app.use(cookieParser());
 
-app.use("/", authRoute);
+app.use("/", () => {
+  authRoute;
+  console.log("welcome to icarus");
+});
 app.use("/user", userRoute);
 
 const port = process.env.PORT || 4000;
